6ff5c88ca4bf8d10c8f60a65dbca8e9e11ec4c29
Agustín Comolli <agustincomolli@gmail.com>
2025-05-19 18:58:08 -0300

    feat: refactor CartDetailItem to use CSS modules for styling

--- src/components/CartDetailItem/CartDetailItem.jsx
+++ src/components/CartDetailItem/CartDetailItem.jsx
@@ -3,24 +3,24 @@ import styles from "./CartDetailItem.module.css";
 export default function CartDetailItem() {
   return (
     <div>
-      <div className="product-image">
+      <div className={styles.image}>
         <img src="../assets/images//martin-fierro.webp" alt="Martin Fierro" height="100px"/>
       </div>
-      <div className="product-details">
-        <div className="product-info">
-          <span className="product-title">Martin Fierro</span>
-          <span className="product-price">$ 0,00</span>
+      <div className={styles.details}>
+        <div className={styles.info}>
+          <span className={styles.title}>Martin Fierro</span>
+          <span className={styles.price}>$ 0,00</span>
         </div>
-        <div className="product-quantity">
-          <button className="product-quantity-button" id="decrease" title="Disminuir"><i className='bx bx-minus'></i></button>
-          <span className="product-quantity-value">1</span>
-          <button className="product-quantity-button" id="increase" title="Aumentar"><i className='bx bx-plus'></i></button>
+        <div className={styles.quantity}>
+          <button className={styles.quantityButton} id="decrease" title="Disminuir"><i className='bx bx-minus'></i></button>
+          <span className={styles.quantityValue}>1</span>
+          <button className={styles.quantityButton} id="increase" title="Aumentar"><i className='bx bx-plus'></i></button>
         </div>
-        <div className="product-total">
-          <span className="product-total-price">$ 0,00</span>
+        <div className={styles.totalWrapper}>
+          <span className={styles.total}>$ 0,00</span>
         </div>
       </div>
-      <button className="product-remove-button" title="Eliminar"><i className='bx bx-trash'></i></button>
+      <button className={styles.removeButton} title="Eliminar"><i className='bx bx-trash'></i></button>
     </div>
   );
 }
